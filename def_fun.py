9dc6dbf13f897294942af5b10b6bf3912d9fa7e6
pullannagari babu <65277372+pullannagari-babu@users.noreply.github.com>
2025-06-25 15:20:57 +0530

    Create def_fun.py

---
+++ def_fun.py
@@ -0,0 +1,22 @@
+cars = [
+    {"make":"ford","model":"cooper","mileage":2113,"fuel_consumed":23},
+    {"make":"volvo","model":"cooper","mileage":123,"fuel_consumed":31}
+    ]
+def car_mileage(car):
+    performance=car["mileage"] / car["fuel_consumed"] 
+    return performance
+
+def car_name(car):
+    name=f"{car['make']} {car['model']}"
+    return name
+    
+
+def print_car_info(car):
+    name=car_mileage(car)
+    performance=car_mileage(car)
+    
+    print(f"{name} does {performance} miles per km")
+    
+    
+for car in cars:
+    km=print_car_info(car)
