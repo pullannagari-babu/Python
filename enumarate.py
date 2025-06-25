59c20ec62e42318bdbb8abe18d629fb06032b994
pullannagari babu <65277372+pullannagari-babu@users.noreply.github.com>
2025-06-25 13:01:02 +0530

    Create enumarate.py

---
+++ enumarate.py
@@ -0,0 +1,14 @@
+names = ["babu","bob","deepa","krisna","yuvansh"]
+
+counter=0
+
+for name in names:
+    print(counter)
+    print(name)
+    counter = counter + 1
+    
+    
+for counter, frd in enumerate(names):
+    print(counter)
+    print(frd)
+    
