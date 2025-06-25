f279ff72305f9cb1b0d4ecee12d9ce7565756eb7
pullannagari babu <65277372+pullannagari-babu@users.noreply.github.com>
2025-06-25 13:02:58 +0530

    Create enumarate-list.py
    [(0, 'babu'), (1, 'bob'), (2, 'deepa'), (3, 'krisna'), (4, 'yuvansh')]

---
+++ enumarate-list.py
@@ -0,0 +1,16 @@
+names = ["babu","bob","deepa","krisna","yuvansh"]
+
+counter=0
+
+for name in names:
+    print(counter)
+    print(name)
+    counter = counter + 1
+    
+    
+for counter, frd in enumerate(names):
+    print(counter)
+    print(frd)
+
+
+print(list(enumerate(names)) // single line output
