b979b414ee8fee84183df4c44d487a8db9611457
pullannagari babu <65277372+pullannagari-babu@users.noreply.github.com>
2025-06-25 12:49:41 +0530

    Create comprhensation_zip.py

---
+++ comprhensation_zip.py
@@ -0,0 +1,11 @@
+name = ["babu","bob","deepa","krisna","yuvansh"]
+ages = [22,35,38,20,18]
+
+my_comprehensation = dict(zip(name,ages))
+
+print(my_comprehensation)
+/*
+
+{'babu': 22, 'bob': 35, 'deepa': 38, 'krisna': 20, 'yuvansh': 18}
+
+*/
